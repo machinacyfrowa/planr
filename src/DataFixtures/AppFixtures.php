44a34007ea3f6bbed48a829b35e79a1fce4cbed0
machinacyfrowa <38609429+machinacyfrowa@users.noreply.github.com>
2019-02-17 22:26:13 +0100

    Update AppFixtures.php
    add automatic creation of roomtypes and rooms, alter room reference in activity class

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,7 +4,9 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
-use App\Entity\ScheduledActivity;
+use App\Entity\Activity;
+use App\Entity\Room;
+use App\Entity\RoomType;
 use \DateTime;
 
 class AppFixtures extends Fixture
@@ -12,7 +14,26 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         for ($i = 0; $i < 20; $i++) {
-            $activity = new ScheduledActivity();
+            //create room types
+            $roomType = new RoomType();
+            $roomType->setName("Random room type" . rand(1,200));
+            $manager->persist($roomType);
+        }
+        $manager->flush();
+        $roomTypes = $manager->getRepository("App:RoomType")->findAll();
+        for ($i = 0; $i < 20; $i++) {
+            //create rooms
+            $room = new Room();
+            $room->setName("Random room " . rand(1,200));
+            $room->setCapacity(rand(10,25));
+            $room->setMaxCapacity(rand(35,40));
+            $room->setRoomType($roomTypes[array_rand($roomTypes)]);
+            $manager->persist($room);
+        }
+        $manager->flush();
+        $rooms = $manager->getRepository("App:Room")->findAll();
+        for ($i = 0; $i < 20; $i++) {
+            $activity = new Activity();
             $randomStartDT = new DateTime();
             $randomStartDT->setTimestamp(time() - rand(1000, 10000));
             $activity->setBegin($randomStartDT);
@@ -20,9 +41,9 @@ class AppFixtures extends Fixture
             $randomEndDT->setTimestamp(time() + rand(1000, 10000));
             $activity->setEnd($randomEndDT);
             $activity->setName("Random activity " . rand(1,200));
-            $activity->setClassroom("Random classroom " . rand(1,200));
             $activity->setStudentGroup("Random group" . rand(1,200));
             $activity->setLecturer("Random lecturer " . rand(1,200));
+            $activity->setRoom($rooms[array_rand($rooms)]);
             $manager->persist($activity);
         }
         $manager->flush();
